06dac6c5c7f4bb9b89e479e57f9ea377ef5d6f24
huangjingliang003 <huangjingliang003@ke.com>
2021-10-20 11:12:29 +0800

    动态规划之矩阵链乘法

---
+++ 算法/src/动态规划/矩阵链乘法.java
@@ -0,0 +1,40 @@
+package 动态规划;
+//矩阵链乘法是指当一些矩阵相乘时，如何加括号来改变乘法顺序从而来降低乘法次数。
+//例如有三个矩阵连乘：A1*A2*A3，其维数分别为：10*100，100*5，5*50.
+//如果按照（（A1*A2）A3）来计算的话，求（A1*A2）要10*100*5=5000次乘法，再乘以A3需要10*5*50=2500次乘法，因此总共需要7500次乘法。
+//如果按照（A1（A2*A3））来计算的话，求（A2*A3）要100*5*50=25000次乘法，再乘以A1需要10*100*50=50000次乘法，因此总共需要75000次乘法。
+
+//A1 30*35 A2 35*15 A3 15*5 A4 5*10 A5 10*20 A6 20*25，则p[0-6]={30,35,15,5,10,20,25}，即Ai=p[i-1]*p[i],i=1:6
+//设分割点在Ak，则当前最优解结构为min{A(0:k-1)*Ak*A(k+1:n-1),A(0:k-1)*(Ak*A(k+1:n-1))}
+
+//但是题解是另一种思路，只划分成了A1*A2*A3……*Ak和A(k+1)*A(k+2)*A(k+3)……*An两部分
+//用一个二维数组r[n+1][n+1]保存结果，下标表示起点和终点,Ak=p[k-1]*p[k],A1*A2*A3……*Ak得到p[i-1]*p[k]矩阵，后者得到p[k]*p[j]矩阵
+//递推关系为r[i][j]=min{r[i,k]+r[k+1,j]+p[i-1]p[k]p[j]，i<=k<j}
+public class 矩阵链乘法 {
+    public static void main(String[] args) {
+        int[] p = new int[]{30, 35, 15, 5, 10, 20, 25};
+        int n = p.length - 1;
+        System.out.println(recur(p, n));
+    }
+
+    public static int recur(int[] p, int n) {
+        int[][] result = new int[n + 1][n + 1];
+        for (int i = 1; i < n + 1; i++) {
+//            单一个不用计算
+            result[i][i] = 0;
+        }
+//        开区间，确保i<j
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n + 1; j++) {
+                int temp_min = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    if (temp_min > result[i][k] + result[k + 1][j] + p[i - 1] * p[k] * p[j]) {
+                        temp_min = result[i][k] + result[k + 1][j] + p[i - 1] * p[k] * p[j];
+                    }
+                }
+                result[i][j] = temp_min;
+            }
+        }
+        return result[1][n];
+    }
+}
--- 算法/src/动态规划/钢条切割.java
+++ 算法/src/动态规划/钢条切割.java
@@ -11,7 +11,7 @@ public class 钢条切割 {
     public static void main(String[] args) {
         int[] price = new int[]{1, 5, 8, 9, 10, 17, 17, 20, 24, 25};
         int n = 10;
-        System.out.println(recur(price,n));
+        System.out.println(recur(price, n));
     }
 
     private static int recur(int[] price, int n) {
@@ -21,9 +21,12 @@ public class 钢条切割 {
             r[i] = Integer.MIN_VALUE;
             int temp_max = Integer.MIN_VALUE;
             for (int j = 1; j <= i; j++) {
-                temp_max = temp_max > price[j-1] + r[i - j] ? temp_max : price[j-1] + r[i - j];
+                if(temp_max <= price[j - 1] + r[i - j]){
+                    temp_max=price[j - 1] + r[i - j];
+                }
             }
             r[i] = temp_max;
+            System.out.println(String.format("长度为%d最优解", i) + r[i]);
         }
         return r[n];
     }
